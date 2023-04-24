515253cbf133133270d633fbf48b43f722fe1f56
Wafula Nasombi <71706858+WafulaNasombi@users.noreply.github.com>
2023-04-25 00:11:29 +0300

    Delete resources/views/api directory

--- resources/views/api/bookid.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-
-@section('content')
-    <h1>{{ $book->title }}</h1>
-    <p>Author: {{ $book->author->fname }} {{ $book->author->lname }}</p>
-    <p>Publication Date: {{ $book->published_date }}</p>
-    <p>ISBN: {{ $book->isbn }}</p>
-   
-@endsection
