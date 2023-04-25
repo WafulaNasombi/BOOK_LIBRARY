7c676356ca497df296f3ce8ac4d27e26d8b5c7f6
Wafula Nasombi <71706858+WafulaNasombi@users.noreply.github.com>
2023-04-25 23:17:23 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,15 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\PublisherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/book', [BookController::class, 'index'])->name('book.index');
-Route::get('/book/{id}', [BookController::class, 'show'])->name('bookid.show');
-Route::post('/book', [BookController::class, 'store'])->name('book.store');
-Route::put('/book/{id}', [BookController::class, 'update'])->name('book.update');
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/book/{id}', [BookController::class, 'show'])->name('books.show');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::put('/book/{id}', [BookController::class, 'update'])->name('books.update');
 
 Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
-Route::get('/author/{id}', [AuthorController::class, 'show'])->name('authorid.show');
-Route::post('/authors', [AuthorController::class, 'store'])->name('author.store');
-Route::put('/author/{id}', [AuthorController::class, 'update'])->name('author.update');
-
+Route::get('/author/{id}', [AuthorController::class, 'show'])->name('authors.show');
+Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store');
+Route::put('/author/{id}', [AuthorController::class, 'update'])->name('authors.update');
 
 
 
